bb83e22bb1755f773d3e5e40354fcd0c04406882
Balince <mbal139@gmail.com>
2019-06-20 23:52:40 -0400

    Initial commit

---
+++ assets/javascript/app.js
@@ -0,0 +1,40 @@
+corrAns = 0;
+incorrAns = 0;
+noAns = 0;
+timeL = 0;
+
+var qnA = [ 
+{
+ques1: "Who is the starting center for the Philadelphia 76ers?", 
+ans1: "Boban Marjanovich",
+ans2: "Joel Embiid", // correct answer
+ans3: "Amir Johnson",
+ans4: "Jonah Bolden"
+},
+{
+ques2: "Who led the league in scoring?",
+ans1b: "Steph Curry",
+ans2b: "Kawhi Leonard",
+ans3b: "James Harden", // correct answer
+ans4b: "LeBron James"
+}
+];
+
+for (i=0; i < qnA.length; i++);
+    $(qnA).text("inputs");
+
+
+    
+     
+     
+
+
+
+function answers(mc1) {
+    
+
+    if (mc1 === c2)
+    corrAns++;
+    else
+    incorrAns++;
+}
